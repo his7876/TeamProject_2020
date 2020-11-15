acb10e0e9b560ad80ed085bcaa672480cceac0c5
hxerimione <purejang98@gmail.com>
2020-11-15 19:08:58 +0900

    JAVA 코드

---
+++ src/Model/Controller.java
@@ -0,0 +1,92 @@
+package Model;
+
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+//import java.sql.*;
+//import java.util.*;
+
+
+public class Controller{
+   WakeOnLan wol;
+
+   //execCNN에서 해당 사원 index return
+   int em_index;
+   static String buff = new String();
+   
+   public Controller() {}
+
+   //얼굴인식+WOL 실행
+   public void getImage(int num) {
+
+         String [] command = new String[2];
+         String index = new String();
+         int len;
+         //int i_index; //사원 인덱스
+         
+         command[0] = "python";
+         command[1] = "C:\\Users\\Hong\\Desktop\\대학교\\학교\\2020 2학기\\산협프\\Face_Recognition\\webcam+recog.py";
+         
+         //CNN model 실행
+         try {
+              execCNN(command);
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+         
+         
+         len = buff.length();
+          index =  buff.substring(len-3);
+          index = index.substring(0,1);
+          //System.out.println(index);
+          em_index = Integer.parseInt(index);
+          //System.out.println(em_index);
+
+          //사원 인덱스 기준으로 DB에 출근 시간 기록
+          DbManager db=new DbManager();
+          if(num==1) {
+        	  db.Commute_On(em_index);
+          }
+          else if(num==2) {
+        	  db.Commute_Off(em_index);
+          }
+          //WOL 실행
+          wol.power_on(em_index);
+   }
+
+   //CNN 모델 실행
+   public static void execCNN(String[] command) throws IOException, InterruptedException{
+        CommandLine commandLine = CommandLine.parse(command[0]);
+         for (int i = 1, n = command.length; i<n; i++) {
+            commandLine.addArgument(command[i]);
+         
+         }
+         
+         
+         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+         PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(outputStream);
+         DefaultExecutor executor = new DefaultExecutor();
+         
+         executor.setStreamHandler(pumpStreamHandler);
+         
+         //executor.setExitValue(1);
+         int result = executor.execute(commandLine);
+         //executor.setExitValue(1);
+         
+         
+         
+         System.out.println("result:" + result);
+         System.out.println("output:" + outputStream.toString());
+         buff = outputStream.toString();
+         
+      }
+
+   
+   
+
+}
---
+++ src/Model/FaceMain.java
@@ -0,0 +1,94 @@
+package Model;
+import java.sql.*;
+import java.util.*;
+
+
+public class FaceMain {
+	public static Calendar cal = Calendar.getInstance();
+	public static Connection conn=null;
+	public static Statement stmt=null;
+	public static ResultSet rs=null;
+	public static PreparedStatement pstmt=null;
+	public static ArrayList<String> ID=new ArrayList<String>();
+	public static ArrayList<String> PW=new ArrayList<String>(); 
+	public static ArrayList<Integer> Num=new ArrayList<Integer>();
+	public static String id;
+	public static String pw;
+	public static int num;
+	public static int index;
+	DbManager db=new DbManager();
+
+	public static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		DbManager db=new DbManager();
+		System.out.println("1.로그인 2.회원가입");
+		int select=sc.nextInt();
+		int check1=1;
+		int check2=0;
+		if(select==1) { //로그인
+			while(check1==0)
+				System.out.println("아이디 입력");
+				id=sc.next();
+				System.out.println("비밀번호 입력");
+				pw=sc.next();
+				check1=db.LogIn(id,pw);
+		}else if(select==2) { //회원가입
+			while(check2!=1) {
+				System.out.println("아이디 입력");
+				id=sc.next();
+				System.out.println("비밀번호 입력");
+				pw=sc.next();
+				System.out.println("사원번호 입력");
+				index=sc.nextInt();
+				check2=db.managerSignUp(id,pw,index);
+				
+			}
+		}
+		while(true) {
+			
+		System.out.println("1.출근 2.퇴근 3.관리");
+		select=sc.nextInt();
+		Controller cont =new Controller();
+		
+		switch(select) {
+		case 1:cont.getImage(1);
+		break;
+		case 2:cont.getImage(2);
+		break;
+		case 3:RunManage();
+		break;
+		}
+		}
+	}
+		
+	
+	
+	public static void RunManage() { //관리자 메뉴
+		DbManager db=new DbManager();
+		System.out.println("1.사원삭제 2.사원추가");
+		int select=sc.nextInt();
+		System.out.println("사원번호  입력");
+		int Employee_IDX=sc.nextInt();
+		int check3=0;
+		switch(select) {
+		case 1:
+			db.deleteEmployee(Employee_IDX);
+		case 2:
+			while(check3!=1) {
+			System.out.println("이름  입력");
+			String Employee_NM=sc.next();
+			System.out.println("부서  입력");
+			String Employee_DP=sc.next();
+			String Employee_CP=null; //컴퓨터경로
+			check3=db.employeeSignUp(Employee_IDX,Employee_NM, Employee_DP,Employee_CP);
+		
+			}
+		}
+	}
+	
+	
+	
+	
+	
+
+}
